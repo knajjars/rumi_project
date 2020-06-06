0ce9c006502997aab5dc5bd713e7733b3ecbb3c7
Khalil Najjar <knajjars@gmail.com>
2020-06-06 11:05:45 -0600

    search feature

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .choices import bedroom_choices, price_choices, state_choices
+from .models import Listing
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by("price").filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page_number = request.GET.get('page')
+    page_listings = paginator.get_page(page_number)
+
+    context = {
+        "listings": page_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        "listing": listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices
+    }
+    return render(request, 'listings/search.html', context)
